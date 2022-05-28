3dcb9761a94f36ffb7e72fc82620490dea651b78
Jannie Yu <jannie@caltech.edu>
2022-05-28 15:38:07 -0700

    Add example routes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # cs132-final
 
 Final project to create a jewelry e-commerce store.
+
+Run front-end
+`python3 -m http.server`
+
+Run back-end
+`nodemon app.js`
--- app.js
+++ app.js
@@ -1,14 +1,50 @@
-/**
- * Example of using our first core module (http) to create a simple http server.
- */
 "use strict";
-const http = require("http");
+const express = require("express");
+const app = express();
 
-let server = http.createServer((request, response) => {
+// http://localhost:8000/hello
+app.get("/hello", function (req, res) {
+  res.type("text"); // same as above
+  res.send("Hello World!");
+});
+
+//localhost:8000/
+http: app.get("/", (req, res) => {
   console.log("I got your request!");
-  response.write("Hello from a Node Server!");
-  response.end();
+  // res.json also sets the response type to application/json appropriately
+  res.json({ msg: ["Hello", "world"] });
+});
+
+// http://localhost:8000/jannie
+app.get("/:name", (req, res) => {
+  res.type("text"); // don't forget to set the response type to change from HTML default
+  let name = req.params["name"];
+  res.send(`Hello ${name}!`);
+});
+
+// Example request using query parameters (optional for this path)
+
+app.get("/cityInfo", (req, res) => {
+  res.type("text");
+  let state = req.query["state"];
+  let city = req.query["city"];
+  if (state && city) {
+    res.send(`State: ${state}, city: ${city}`);
+  } else {
+    res.status(400).send("Missing required state and city parameters.");
+  }
+});
+
+// http://localhost:8000/states/CA/cities/SD
+// Example request using path parameters instead (required to hit this route path)
+// This is generally more common for Express APIs than the above option.
+app.get("/states/:state/cities/:city", (req, res) => {
+  let state = req.params["state"];
+  let city = req.params["city"];
+  res.json({ state: state, city: city });
 });
 
 const PORT = process.env.PORT || 8000;
-server.listen(PORT);
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT + "...");
+});
