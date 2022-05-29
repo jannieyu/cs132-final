503bbf52364611c8daddc6ef0fb24dbe2c55506e
jennie6 <jenniechung6@gmail.com>
2022-05-28 21:29:58 -0700

    added faq

--- setup.sql
+++ setup.sql
@@ -14,9 +14,9 @@ CREATE TABLE jewelry(
   id            SERIAL PRIMARY KEY, 
   -- Product name should be descriptive, i.e. should adequately describe what the 
   -- jewelry item is
-  product_name   VARCHAR(255)  NOT NULL,  
+  product_name   VARCHAR(200)  NOT NULL,  
   -- Describes the exact image path of the jewelry item
-  img_path       VARCHAR(255)  NOT NULL,
+  img_path       VARCHAR(200)  NOT NULL,
   -- The type of jewelry, e.g. bracelet, necklace, ring, etc.
   prod_type      VARCHAR(30)   NOT NULL,
   -- The price of the jewelry item, should have two decimal places
@@ -27,6 +27,28 @@ CREATE TABLE jewelry(
   style         VARCHAR(30)   NOT NULL,
   CHECK (style IN ('casual', 'formal'))
 );
+
+-- Create a table for contact information
+CREATE TABLE contact_info(
+  -- Unique identifier for the contact table entry
+  contact_id      SERIAL PRIMARY KEY,
+  -- email address for the contact
+  email           VARCHAR(50)   NOT NULL, 
+  -- time that the entry was made
+  time_submitted  DATETIME NOT NULL,
+  -- The contact message body, i.e. what the user is reaching out for
+  contact_msg     VARCHAR(500)  NOT NULL
+)
+
+-- Create a table for FAQs
+CREATE TABLE faq(
+  -- Unique identifier for the FAQ
+  faq_id SERIAL PRIMARY KEY,
+  -- Question which is asked 
+  question VARCHAR(200)  NOT NULL,
+  -- Answer for given question
+  answer VARCHAR(200)    NOT NULL
+)
 
 -- Format of insert statement: 
 -- INSERT INTO jewelry VALUES(id, product_name, img_path, prod_type, price, color, 
@@ -52,3 +74,18 @@ INSERT INTO jewelry VALUES(12, 'Silver Drop Necklace', 'img/necklaces/drop_neckl
 INSERT INTO jewelry VALUES(13, 'Thin Moon Cresent Necklace', 'img/necklaces/moon_necklace.jpg','necklace',19.99, 'pink', 'casual');
 INSERT INTO jewelry VALUES(14, 'Gold Ruby Necklace', 'img/necklaces/ruby_necklace.jpg','necklace', 29.99, 'red', 'casual');
 INSERT INTO jewelry VALUES(15, 'Sweetheart Necklace', 'img/necklaces/sweetheart_necklace.jpg','necklace', 59.99, 'silver', 'formal');
+
+-- Format of FAQ entries
+-- INSERT_INTO faq VALUES(faq_id, question, anwer)
+INSERT INTO faq VALUES (1, "How long does shipping take?", "Shipping typically takes 2 weeks for U.S. orders. 
+  We don't ship internationally yet, but we're hoping to expand soon!");
+INSERT INTO faq VALUES (2, "What was the inspiration for this store?", "We made a jewelry store because 
+  high-quality jewelry is typically hard to come by at affordable prices. We try to provide high-quality
+  items at prices that are significantly lower than other brands.");
+INSERT INTO faq VALUES (3, "What kind of jewelry do you guys offer?", "We offer a variety of rings, necklaces,
+  and earrings with precious metals and stones. We hope to offer bracelets and more niche jewelry types
+  in the future!");
+INSERT INTO faq VALUES (4, "What's your return policy?", "We will take returns up to one month (30 days) after
+  purchase. After this period, returns cannot be made.")
+INSERT INTO faq VALUES (5, "Can I buy this jewelry in person?", "Unfortunately, no. We only offer our goods online.
+  We hope to open in-person stores within the next 5 years!")
