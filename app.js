0f349ef17955804eaf7569b2f71b588f6ff8e431
Jannie Yu <jannie@caltech.edu>
2022-05-30 14:53:35 -0700

    update middleware error handling and improve app.js comments

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
  * GET /jewelry/random
  * GET /jewelry?type=__&price=__&color=__&date-listed=__&style=__
  * GET /faq
+ *
+ * POST /contact
  */
 
 "use strict";
@@ -18,10 +20,7 @@ const multer = require("multer");
 
 const SERVER_ERR_CODE = 500;
 const CLIENT_ERR_CODE = 400;
-const SERVER_ERROR =
-  "Something went wrong on the server, please try again later.";
-const CLIENT_ERROR =
-  "Bad client request; missing parameters. Please try again.";
+const SERVER_ERROR = "Internal sever error, please try again later.";
 const NO_INPUT = [];
 
 // for parsing application/x-www-form-urlencoded
@@ -56,8 +55,22 @@ async function getDB() {
   return db;
 }
 
-// Endpoint to get all jewelry
-app.get("/jewelry", async function (req, res) {
+/**
+ * Returns an JSON array of jewelry with the following filters:
+ * type, color, style, price.
+ * Example:
+ * [{"id":1,
+ * "product_name":"Dual Band Ring",
+ * "descrip":"Why have one when you can have two?",
+ * "img_path":"img/rings/dual_band_ring.jpg",
+ * "prod_type":
+ * "ring","price":29.99,
+ * "color":"gold",
+ * "style":"casual"}, ...]
+ * Returns 400 error if invalid GET parameters.
+ * Returns 500 error if internal server errors.
+ */
+app.get("/jewelry", async function (req, res, next) {
   res.type("json");
   let type = req.query["type"];
   let color = req.query["color"];
@@ -66,8 +79,7 @@ app.get("/jewelry", async function (req, res) {
   let input = [];
   let queryRestrictions = [];
 
-  // Append to the input list as necessary depending on which query parameters
-  // are specified
+  // Append to the input list depending on which query parameters are specified
   let qry = "SELECT * FROM jewelry";
   if (type || color || style || priceLimit) {
     qry += " WHERE ";
@@ -88,33 +100,41 @@ app.get("/jewelry", async function (req, res) {
       input.push(priceLimit);
     }
   }
-
   qry += queryRestrictions.join(" AND ");
 
-  // Trying to extract the promisified query results, otherwise use error
-  // handling
+  // Extract the promisified query results, otherwise handle error
   let db;
   try {
-    db = await getDB(); // connection error thrown in getDB();
+    db = await getDB();
     let rows;
     if (input == NO_INPUT) {
       rows = await db.query(qry);
     } else {
       rows = await db.query(qry, input);
     }
-    if (rows.length !== 0) {
-      res.send(rows);
-    } else {
-      res.status(CLIENT_ERR_CODE).send(CLIENT_ERROR);
-    }
     db.end();
   } catch (err) {
-    res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    res.status(SERVER_ERR_CODE);
+    err.message = SERVER_ERROR;
+    next(err);
   }
 });
 
-// Endpoint to get information about just one product using id
-app.get("/jewelry/:id", async function (req, res) {
+/**
+ * Returns an JSON array of one jewelry, queried by item ID.
+ * Example:
+ * [{"id":1,
+ * "product_name":"Dual Band Ring",
+ * "descrip":"Why have one when you can have two?",
+ * "img_path":"img/rings/dual_band_ring.jpg",
+ * "prod_type":
+ * "ring","price":29.99,
+ * "color":"gold",
+ * "style":"casual"}]
+ * Returns 400 error if invalid GET parameter ID value.
+ * Returns 500 error if internal server errors.
+ */
+app.get("/jewelry/:id", async function (req, res, next) {
   res.type("json");
   let qry = "SELECT * FROM jewelry WHERE id=?";
   let input = [req.params.id];
@@ -123,19 +143,29 @@ app.get("/jewelry/:id", async function (req, res) {
   try {
     db = await getDB();
     let rows = await db.query(qry, input);
-    // Check for empty values and return 200/400 error
+    db.end();
+    // Check for invalid item ID
     if (rows.length !== 0) {
       res.send(rows);
     } else {
-      res.status(CLIENT_ERR_CODE).send(CLIENT_ERROR);
+      res.status(CLIENT_ERR_CODE);
+      next(Error("Bad client request: jewelry does not exist."));
     }
-    db.end();
   } catch (err) {
-    res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    res.status(SERVER_ERR_CODE);
+    err.message = SERVER_ERROR;
+    next(err);
   }
 });
 
-// Endpoint used to post information from a contact request
+/**
+ * Endpoint to post contact information filled out by user.
+ * Required POST parameters: name, email, message.
+ * Response type: text/plain
+ * Returns 400 error if missing POST parameters
+ * Sends a 500 error if DB failed
+ * Sends a success message otherwise.
+ */
 app.post("/contact", async function (req, res, next) {
   res.type("text");
   let name = req.body.name;
@@ -155,22 +185,34 @@ app.post("/contact", async function (req, res, next) {
 
   if (!(name && email && message && timestamp)) {
     res.status(CLIENT_ERR_CODE);
-  }
-  // Trying to extract the promisified query results, otherwise use error
-  // handling
-  let db;
-  try {
-    db = await getDB();
-    let rows = await db.query(qry, input);
-    res.send("Message successfully sent! We will look into it shortly.");
-    db.end();
-  } catch (err) {
-    res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    next(
+      Error(
+        "Bad client request: empty POST parameters name, email, or message in /contact."
+      )
+    );
+  } else {
+    // Trying to extract the promisified query results, otherwise use error
+    // handling
+    let db;
+    try {
+      db = await getDB();
+      let rows = await db.query(qry, input);
+      db.end();
+      res.send("Message successfully sent! We will look into it shortly.");
+    } catch (err) {
+      res.status(SERVER_ERR_CODE);
+      err.message = SERVER_ERROR;
+      next(err);
+    }
   }
 });
 
-// Endpoint to get random jewelry set
-app.get("/random", async function (req, res) {
+/**
+ * Returns an JSON array of a random jewelry set
+ * with ring, necklace, earring item
+ * Returns 500 error if internal server errors.
+ */
+app.get("/random", async function (req, res, next) {
   res.type("json");
 
   // Return a random set of jewelry
@@ -195,36 +237,59 @@ app.get("/random", async function (req, res) {
   try {
     db = await getDB();
     let rows = await db.query(qry);
+    db.end();
+
     if (rows.length !== 0) {
       res.send(rows);
     } else {
-      res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+      res.status(SERVER_ERR_CODE);
+      err.message = SERVER_ERROR;
+      next(err);
     }
-    db.end();
   } catch (err) {
-    res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    res.status(SERVER_ERR_CODE);
+    err.message = SERVER_ERROR;
+    next(err);
   }
 });
 
-// Endpoint to get faq
-app.get("/faq", async function (req, res) {
+/**
+ * Returns an JSON array of a FAQ questions & answers.
+ * Example:
+ * [{"faq_id":1,
+ * "question":"How long does shipping take?",
+ * "answer":"Shipping typically \ntakes 2 weeks for U.S. orders.
+ * We don't ship internationally yet, but we're \nhoping to expand soon!"}...]
+ * Returns 500 error if DB fails.
+ */
+app.get("/faq", async function (req, res, next) {
   res.type("json");
   let qry = "SELECT * FROM faq";
 
-  // Trying to extract the promisified query results, otherwise use error
-  // handling
+  // Extract the promisified query results, otherwise handle error
   let db;
   try {
     db = await getDB();
-    let val = await db.query(qry);
-    // Check for empty values and return 200/400 error
-    if (val.length !== 0) {
-      res.send(val);
-    } else {
-      res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
-    }
+    let rows = await db.query(qry);
     db.end();
+
+    // Check for empty values and return 200/400 error
+    if (rows.length !== 0) {
+      res.send(rows);
+    } else {
+      res.status(SERVER_ERR_CODE);
+      err.message = SERVER_ERROR;
+      next(err);
+    }
   } catch (err) {
-    res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    res.status(SERVER_ERR_CODE);
+    err.message = SERVER_ERROR;
+    next(err);
   }
+});
+
+// Returns error message for middleward stack error handling in plain text.
+app.use((err, req, res, next) => {
+  res.type("text");
+  res.send(err.message);
 });
